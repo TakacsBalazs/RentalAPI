55a201256a030a9bb69cca86697edf14ee3d58d9
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-12 19:20:15 +0100

    Added user model with FullName

---
+++ Rental.API/Models/User.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Rental.API.Models
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+    }
+}
