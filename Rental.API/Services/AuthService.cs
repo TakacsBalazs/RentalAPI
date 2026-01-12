2401be3379b127ebdfb7059582e3913aa056fdfe
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-12 20:01:38 +0100

    Created AuthService implements IAuthService added UserManager and IConfiguration

---
+++ Rental.API/Services/AuthService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Rental.API.Common;
+using Rental.API.Models;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<User> userManager;
+        private readonly IConfiguration configuration;
+
+        public AuthService(UserManager<User> userManager, IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+
+        public Task<Result> RegisterAsync(CreateUserRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
