9f18b05c154677ef439580a0d15d8a1e1a54adb8
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-12 19:20:48 +0100

    Created AppDbContext and added User fullname required and max 100

---
+++ Rental.API/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Rental.API.Models;
+
+namespace Rental.API.Data
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity =>
+            {
+                entity.Property(x => x.FullName).IsRequired().HasMaxLength(100);
+            });
+        }
+
+    }
+}
