2d82277293bf84cc59b75f737c158d255fbc7f44
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-13 12:19:03 +0100

    Created AuthService and added RegisterAsync

--- Rental.API/Services/AuthService.cs
+++ Rental.API/Services/AuthService.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Rental.API.Common;
+using Rental.API.Extensions;
 using Rental.API.Models;
 using Rental.API.Models.Requests;
 
@@ -9,16 +11,38 @@ namespace Rental.API.Services
     {
         private readonly UserManager<User> userManager;
         private readonly IConfiguration configuration;
+        private readonly IServiceProvider serviceProvider;
 
-        public AuthService(UserManager<User> userManager, IConfiguration configuration)
+        public AuthService(UserManager<User> userManager, IConfiguration configuration, IServiceProvider serviceProvider)
         {
             this.userManager = userManager;
             this.configuration = configuration;
+            this.serviceProvider = serviceProvider;
         }
 
-        public Task<Result> RegisterAsync(CreateUserRequest request)
+        public async Task<Result> RegisterAsync(CreateUserRequest request)
         {
-            throw new NotImplementedException();
+            var validate = await serviceProvider.ValidateRequestAsync<CreateUserRequest>(request);
+            if(!validate.IsSuccess)
+            {
+                return validate;
+            }
+
+            var user = new User
+            {
+                Email = request.Email,
+                UserName = request.UserName,
+                FullName = request.FullName,
+            };
+
+            var result = await userManager.CreateAsync(user, request.Password);
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors.Select(x => x.Description).ToList();
+                return Result.Failure(errors);
+            }
+
+            return Result.Success();
         }
     }
 }
