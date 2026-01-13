563ba5fc8024517edb79448698e6bbb52c324bd1
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-13 12:16:36 +0100

    Created ValidateExtensions and added ValidateRequestAsync

---
+++ Rental.API/Extensions/ValidateExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Rental.API.Common;
+
+namespace Rental.API.Extensions
+{
+    public static class ValidationExtensions
+    {
+        public static async Task<Result> ValidateRequestAsync<T>(this IServiceProvider provider, T request)
+        {
+            var validator = provider.GetService<IValidator<T>>();
+
+            if (validator == null) return Result.Success();
+
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (!validationResult.IsValid)
+            {
+                var errors = validationResult.Errors.Select(e => e.ErrorMessage).ToList();
+                return Result.Failure(errors);
+            }
+
+            return Result.Success();
+        }
+    }
+}
