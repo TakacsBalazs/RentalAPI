2b05ac2565c6a094b209047f8f7d20936dd20fb1
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-13 12:20:42 +0100

    Created AuthController and added register post

---
+++ Rental.API/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] CreateUserRequest request)
+        {
+            var result = await authService.RegisterAsync(request);
+            if(!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("Successfull registration!");
+        }
+    }
+}
