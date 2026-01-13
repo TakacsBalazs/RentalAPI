f494aca148a814d0e5f126df8b60d0c2a0ef490a
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-13 12:14:38 +0100

    Created CreateUserRequestValidator

---
+++ Rental.API/Validators/CreateUserRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class CreateUserRequestValidator : AbstractValidator<CreateUserRequest>
+    {
+        public CreateUserRequestValidator()
+        {
+            RuleFor(x => x.FullName).NotEmpty().MaximumLength(100);
+            RuleFor(x => x.UserName).NotEmpty().MinimumLength(3).MaximumLength(20);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+            RuleFor(x => x.Password).NotEmpty();
+            RuleFor(x => x.ConfirmPassword).Equal(x => x.Password);
+        }
+    }
+}
