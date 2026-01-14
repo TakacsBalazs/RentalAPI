81daf8466ff2bc05a18c78f41e46fb4132768eca
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 15:55:49 +0100

    IAuthService: added LoginAsync

--- Rental.API/Services/IAuthService.cs
+++ Rental.API/Services/IAuthService.cs
@@ -1,10 +1,13 @@
 ﻿using Rental.API.Common;
 using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
 
 namespace Rental.API
 {
     public interface IAuthService
     {
         Task<Result> RegisterAsync(CreateUserRequest request);
+
+        Task<Result<UserLoginResponse>> LoginAsync(UserLoginRequest request);
     }
 }
