ddbc67e0ab899d6b7f1901b2363a51463abd125c
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 20:59:00 +0100

    Created ToolService and added CreateToolAsync

---
+++ Rental.API/Services/ToolService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Rental.API.Common;
+using Rental.API.Data;
+using Rental.API.Extensions;
+using Rental.API.Models;
+using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public class ToolService : IToolService
+    {
+        private readonly AppDbContext context;
+        private readonly IServiceProvider serviceProvider;
+
+        public ToolService(AppDbContext context, IServiceProvider serviceProvider)
+        {
+            this.context = context;
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task<Result<CreateToolResponse>> CreateToolAsync(CreateToolRequest request, string userId)
+        {
+            var validate = await serviceProvider.ValidateRequestAsync<CreateToolRequest>(request);
+            if (!validate.IsSuccess)
+            {
+                return Result<CreateToolResponse>.Failure(validate.Errors);
+            }
+
+            var tool = new Tool
+            {
+                Name = request.Name,
+                DailyPrice = request.DailyPrice,
+                UserId = userId,
+                SecurityDeposit = request.SecurityDeposit,
+                Description = request.Description,
+                Category = request.Category,
+                Location = request.Location
+            };
+
+            context.Tools.Add(tool);
+            await context.SaveChangesAsync();
+
+            var response = new CreateToolResponse
+            {
+                Id = tool.Id,
+                Name = tool.Name,
+                UserId = tool.UserId,
+                IsActive = tool.IsActive,
+                Description = tool.Description,
+                Category = tool.Category,
+                Location = tool.Location,
+                DailyPrice = tool.DailyPrice,
+                SecurityDeposit = tool.SecurityDeposit
+            };
+
+            return Result<CreateToolResponse>.Success(response);
+        }
+    }
+}
