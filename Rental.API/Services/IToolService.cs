2eda391f851931e3c68bc41c42dc369b833dd508
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 20:58:41 +0100

    Created IToolService and added CreateToolAsync

---
+++ Rental.API/Services/IToolService.cs
@@ -0,0 +1,12 @@
+﻿using Rental.API.Common;
+using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public interface IToolService
+    {
+
+        Task<Result<CreateToolResponse>> CreateToolAsync(CreateToolRequest request, string userId);
+    }
+}
