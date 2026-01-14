bb274fbff20879776327e176ff2d4e33087901d8
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 20:57:51 +0100

    Created CreateToolRequest

---
+++ Rental.API/Models/Requests/CreateToolRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace Rental.API.Models.Requests
+{
+    public class CreateToolRequest
+    {
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal DailyPrice { get; set; }
+        public decimal SecurityDeposit { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public Category Category { get; set; }
+
+    }
+}
