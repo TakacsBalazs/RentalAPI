d80bd68bafdd7e6497214807362b38f75cb3fddb
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 17:27:04 +0100

    User model: added Tools

--- Rental.API/Models/User.cs
+++ Rental.API/Models/User.cs
@@ -7,5 +7,7 @@ namespace Rental.API.Models
         public string FullName { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Tool> Tools { get; set; }
     }
 }
