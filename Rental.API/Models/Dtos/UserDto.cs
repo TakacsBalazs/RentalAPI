8d2cb1b5da39ea47e2c1d38bda1be217833a4aeb
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 21:24:35 +0100

    Created UserDto

---
+++ Rental.API/Models/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental.API.Models.Dtos
+{
+    public class UserDto
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+    }
+}
