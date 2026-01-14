addf38c1a800d25a9a62e68b59038a27f92d985f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 17:26:42 +0100

    Created Tool model

---
+++ Rental.API/Models/Tool.cs
@@ -0,0 +1,33 @@
+﻿namespace Rental.API.Models
+{
+    public class Tool
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+
+        public decimal DailyPrice { get; set; }
+        public decimal SecurityDeposit { get; set; }
+
+        public string Location { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public string UserId { get; set; } = string.Empty;
+        public User User { get; set; }
+
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        CONSTRUCTION,
+        GARDENING,
+        CLEANING,
+        WOODWORKING
+    }
+}
