9f8b13fb92fb5864fdee05c0b9a9d2b13eb0d185
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 21:01:32 +0100

    Created ToolController and added tool create

---
+++ Rental.API/Controllers/ToolController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Models.Requests;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/tool")]
+    [ApiController]
+    public class ToolController : ControllerBase
+    {
+        private readonly IToolService toolService;
+
+        public ToolController(IToolService toolService)
+        {
+            this.toolService = toolService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateTool(CreateToolRequest request)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            var result = await toolService.CreateToolAsync(request, userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
