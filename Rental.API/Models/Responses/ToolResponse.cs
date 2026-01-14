f377c89113a99483b263f4d2df4b6e80beb30623
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 21:24:50 +0100

    Created ToolResponse

---
+++ Rental.API/Models/Responses/ToolResponse.cs
@@ -0,0 +1,18 @@
+﻿using Rental.API.Models.Dtos;
+
+namespace Rental.API.Models.Responses
+{
+    public class ToolResponse
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal DailyPrice { get; set; }
+        public decimal SecurityDeposit { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public Category Category { get; set; }
+        public UserDto User { get; set; }
+    }
+}
