90a3a44311ae7a0dff5713a701b2c953ee24d697
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 15:54:46 +0100

    Created UserLoginResponse

---
+++ Rental.API/Models/Responses/UserLoginResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class UserLoginResponse
+    {
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime Expiration { get; set; }
+    }
+}
