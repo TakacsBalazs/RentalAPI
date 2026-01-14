c774dc1acde92de3caa51e7f0696158866676774
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 15:57:00 +0100

    AuthService added GenerateJwtToken and LoginAsync

--- Rental.API/Services/AuthService.cs
+++ Rental.API/Services/AuthService.cs
@@ -1,9 +1,15 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Rental.API.Common;
 using Rental.API.Extensions;
 using Rental.API.Models;
 using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Rental.API.Services
 {
@@ -43,6 +49,77 @@ namespace Rental.API.Services
             }
 
             return Result.Success();
+        }
+
+        public async Task<Result<UserLoginResponse>> LoginAsync(UserLoginRequest request)
+        {
+            var validate = await serviceProvider.ValidateRequestAsync<UserLoginRequest>(request);
+            if(!validate.IsSuccess)
+            {
+                return Result<UserLoginResponse>.Failure(validate.Errors);
+            }
+
+            User? user = null;
+            if (request.Identifier.Contains('@'))
+            {
+                user = await userManager.FindByEmailAsync(request.Identifier);
+            } else
+            {
+                user = await userManager.FindByNameAsync(request.Identifier);
+            }
+
+            if(user == null)
+            {
+                return Result<UserLoginResponse>.Failure("Invalid email or username!");
+            }
+
+            if (await userManager.IsLockedOutAsync(user))
+            {
+                return Result<UserLoginResponse>.Failure("The account is temporarily locked.");
+            }
+
+            var passwordCheck = await userManager.CheckPasswordAsync(user, request.Password);
+            if (!passwordCheck)
+            {
+                await userManager.AccessFailedAsync(user);
+
+                if(await userManager.IsLockedOutAsync(user))
+                {
+                    return Result<UserLoginResponse>.Failure("The account is temporarily locked.");
+                }
+
+                return Result<UserLoginResponse>.Failure("Invalid password!");
+            }
+
+            await userManager.ResetAccessFailedCountAsync(user);
+
+            var tokenResult = GenerateJwtToken(user);
+            return Result<UserLoginResponse>.Success(tokenResult);
+        }
+
+        private UserLoginResponse GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecurityKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: creds
+            );
+
+            return new UserLoginResponse
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = token.ValidTo
+            };
         }
     }
 }
