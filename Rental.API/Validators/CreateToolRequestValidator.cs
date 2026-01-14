7051b0d48f3fb9158065cf77b57bdf7c039452ce
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 21:01:17 +0100

    Created CreateToolRequestValidator

---
+++ Rental.API/Validators/CreateToolRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class CreateToolRequestValidator : AbstractValidator<CreateToolRequest>
+    {
+
+        public CreateToolRequestValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(10).MaximumLength(100);
+            RuleFor(x => x.Description).NotEmpty().MinimumLength(20).MaximumLength(1000);
+            RuleFor(x => x.DailyPrice).NotEmpty().GreaterThan(0).PrecisionScale(18, 2, true);
+            RuleFor(x => x.SecurityDeposit).NotEmpty().GreaterThan(0).PrecisionScale(18, 2, true);
+            RuleFor(x => x.Location).NotEmpty().MaximumLength(200);
+            RuleFor(x => x.Category).NotEmpty().IsInEnum();
+
+        }
+
+    }
+}
