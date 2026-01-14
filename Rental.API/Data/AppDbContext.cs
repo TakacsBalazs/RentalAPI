8e56a3fc7fc42122a463de98a2192428c9c47c9e
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-14 17:27:36 +0100

    AppDbContext: Added tools and tools settings

--- Rental.API/Data/AppDbContext.cs
+++ Rental.API/Data/AppDbContext.cs
@@ -11,6 +11,8 @@ namespace Rental.API.Data
             
         }
 
+        public DbSet<Tool> Tools { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -20,6 +22,20 @@ namespace Rental.API.Data
                 entity.Property(x => x.FullName).IsRequired().HasMaxLength(100);
                 entity.Property(x => x.UserName).IsRequired().HasMaxLength(20);
                 entity.Property(x => x.CreatedAt).IsRequired().HasDefaultValueSql("GETUTCDATE()");
+            });
+
+            builder.Entity<Tool>(entity =>
+            {
+                entity.Property(x => x.Name).IsRequired().HasMaxLength(100);
+                entity.Property(x => x.DailyPrice).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(x => x.IsActive).IsRequired();
+                entity.Property(x => x.UserId).IsRequired();
+                entity.Property(x => x.SecurityDeposit).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(x => x.Description).IsRequired().HasMaxLength(1000);
+                entity.Property(x => x.Category).IsRequired().HasConversion<string>();
+                entity.Property(x => x.Location).IsRequired().HasMaxLength(200);
+
+                entity.HasOne(x => x.User).WithMany(x => x.Tools).HasForeignKey(x => x.UserId);
             });
         }
 
