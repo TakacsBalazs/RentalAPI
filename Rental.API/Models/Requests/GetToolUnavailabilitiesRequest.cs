30110e714021a800227b575b102386ab04b3b6fd
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 23:34:44 +0100

    Created GetToolUnavailabilitiesRequest

---
+++ Rental.API/Models/Requests/GetToolUnavailabilitiesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Rental.API.Models.Requests
+{
+    public class GetToolUnavailabilitiesRequest
+    {
+        public int ToolId { get; set; }
+
+        public DateOnly? From { get; set; }
+
+        public DateOnly? To { get; set; }
+    }
+}
