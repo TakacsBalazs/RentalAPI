7882172c664b3a08e23749d54b402840a5766025
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 23:35:41 +0100

    Created GetToolUnavailabilitiesRequestValidator

---
+++ Rental.API/Validators/GetToolUnavailabilitiesRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class GetToolUnavailabilitiesRequestValidator : AbstractValidator<GetToolUnavailabilitiesRequest>
+    {
+        public GetToolUnavailabilitiesRequestValidator()
+        {
+            RuleFor(x => x.ToolId).GreaterThan(0);
+            RuleFor(x => x.To).GreaterThanOrEqualTo(x => x.From).When(x => x.From != null && x.To != null);
+        }
+    }
+}
