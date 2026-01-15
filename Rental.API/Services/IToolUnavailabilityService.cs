acf485d6e77ebdcbaebc3dec945fcad41f899f66
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 21:07:23 +0100

    IToolUnavailabilityService and added CreateToolUnavailabilityAsync

---
+++ Rental.API/Services/IToolUnavailabilityService.cs
@@ -0,0 +1,13 @@
+﻿using Rental.API.Common;
+using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public interface IToolUnavailabilityService
+    {
+
+        Task<Result<ToolUnavailabilityResponse>> CreateToolUnavailabilityAsync(CreateToolUnavailabilityRequest request, string id);
+
+    }
+}
