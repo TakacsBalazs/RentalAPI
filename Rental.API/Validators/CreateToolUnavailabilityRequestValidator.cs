b4ffc3659b07bac9be5ee2dc09ea3dbbd6c5f4ee
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 21:08:03 +0100

    Created CreateToolUnavailabilityRequestValidator

---
+++ Rental.API/Validators/CreateToolUnavailabilityRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class CreateToolUnavailabilityRequestValidator : AbstractValidator<CreateToolUnavailabilityRequest>
+    {
+        public CreateToolUnavailabilityRequestValidator()
+        {
+            RuleFor(x => x.StartDate).NotEmpty().GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow));
+            RuleFor(x => x.EndDate).NotEmpty().GreaterThanOrEqualTo(x => x.StartDate);
+            RuleFor(x => x.ToolId).NotEmpty();
+        }
+    }
+}
