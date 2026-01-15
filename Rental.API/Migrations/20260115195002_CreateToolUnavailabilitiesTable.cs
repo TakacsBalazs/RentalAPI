ad491dab53a74085e6784306fe1eb71908dc4d07
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 21:05:24 +0100

    Created CreateToolUnavailabilityTables migration

---
+++ Rental.API/Migrations/20260115195002_CreateToolUnavailabilitiesTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateToolUnavailabilitiesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToolUnavailabilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    ToolId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToolUnavailabilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToolUnavailabilities_Tools_ToolId",
+                        column: x => x.ToolId,
+                        principalTable: "Tools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToolUnavailabilities_ToolId",
+                table: "ToolUnavailabilities",
+                column: "ToolId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToolUnavailabilities");
+        }
+    }
+}
