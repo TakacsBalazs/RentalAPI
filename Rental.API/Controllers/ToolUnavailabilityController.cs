77257e1c2dbe4d43587e3bf6ad641b3c57d1d847
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 21:09:38 +0100

    Created ToolUnavailabilityController and added CreateToolUnavailability

---
+++ Rental.API/Controllers/ToolUnavailabilityController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Models.Requests;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/toolunavailability")]
+    [ApiController]
+    public class ToolUnavailabilityController : ControllerBase
+    {
+        private readonly IToolUnavailabilityService toolUnavailabilityService;
+
+        public ToolUnavailabilityController(IToolUnavailabilityService toolUnavailabilityService)
+        {
+            this.toolUnavailabilityService = toolUnavailabilityService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateToolUnavailability(CreateToolUnavailabilityRequest request)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await toolUnavailabilityService.CreateToolUnavailabilityAsync(request, userId);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok(result.Data);
+        }
+    }
+}
