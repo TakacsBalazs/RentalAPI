9c501187eae5c956cc79d2ef3cef9475775da752
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 21:02:32 +0100

    Created ToolUnavailability model

---
+++ Rental.API/Models/ToolUnavailability.cs
@@ -0,0 +1,14 @@
+﻿namespace Rental.API.Models
+{
+    public class ToolUnavailability
+    {
+        public int Id { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public int ToolId { get; set; }
+        public Tool Tool { get; set; }
+    }
+}
