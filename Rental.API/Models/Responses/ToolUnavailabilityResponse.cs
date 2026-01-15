5b0559d31c0876feb2b99c22985c767a9c962d39
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-15 21:06:45 +0100

    Created ToolUnavailabilityResponse

---
+++ Rental.API/Models/Responses/ToolUnavailabilityResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class ToolUnavailabilityResponse
+    {
+        public int Id { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public int ToolId { get; set; }
+    }
+}
