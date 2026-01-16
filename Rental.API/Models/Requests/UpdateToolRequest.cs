79dee9c25608a6a00b647a02e986e98273bde451
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 12:57:41 +0100

    Created UpdateToolRequest

---
+++ Rental.API/Models/Requests/UpdateToolRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Rental.API.Models.Requests
+{
+    public class UpdateToolRequest
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public decimal DailyPrice { get; set; }
+        public decimal SecurityDeposit { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public Category Category { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        public DateOnly AvailableUntil { get; set; }
+    }
+}
