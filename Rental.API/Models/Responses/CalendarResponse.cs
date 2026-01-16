f2d04c320874cb1d1815ee919eddbad6e3695867
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-17 00:30:36 +0100

    Created CalendarResponse

---
+++ Rental.API/Models/Responses/CalendarResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class CalendarResponse
+    {
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
