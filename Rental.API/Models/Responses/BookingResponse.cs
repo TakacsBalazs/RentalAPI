a7951c1d64eef4129eae15277117988301d7943f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 20:51:23 +0100

    Created BookingResponse

---
+++ Rental.API/Models/Responses/BookingResponse.cs
@@ -0,0 +1,17 @@
+﻿using Rental.API.Models.Dtos;
+
+namespace Rental.API.Models.Responses
+{
+    public class BookingResponse
+    {
+        public int Id { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public BookingStatus Status { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal SecurityDeposit { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public ToolDto Tool { get; set; }
+        public UserDto? Renter { get; set; }
+    }
+}
