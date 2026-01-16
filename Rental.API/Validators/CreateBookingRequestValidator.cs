11e8d264e810cb986a00b5c83fb7faf57be6860e
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 20:52:39 +0100

    Created CreateBookingRequestValidator

---
+++ Rental.API/Validators/CreateBookingRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class CreateBookingRequestValidator : AbstractValidator<CreateBookingRequest>
+    {
+        public CreateBookingRequestValidator() {
+            RuleFor(x => x.ToolId).GreaterThan(0);
+            RuleFor(x => x.StartDate).NotEmpty().GreaterThan(DateOnly.FromDateTime(DateTime.UtcNow));
+            RuleFor(x => x.EndDate).NotEmpty().GreaterThanOrEqualTo(x => x.StartDate);
+        }
+    }
+}
