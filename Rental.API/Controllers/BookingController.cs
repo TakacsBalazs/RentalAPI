f8319dd0d60a61310465b48e0182abdb0fda2230
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 21:12:09 +0100

    BookingController: Added GetAllBookedTools (/booked-tools route)

--- Rental.API/Controllers/BookingController.cs
+++ Rental.API/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Rental.API.Models.Requests;
@@ -24,6 +25,19 @@ namespace Rental.API.Controllers
         {
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
             var result = await bookingService.CreateBookingAsync(request, userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+
+        [Authorize]
+        [HttpGet("booked-tools")]
+        public async Task<IActionResult> GetAllBookedTools()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await bookingService.GetAllBookedToolsAsync(userId);
             if (!result.IsSuccess)
             {
                 return BadRequest(result.Errors);
