0bc1ece43674a14c7a2adcb6f6198a11906c4f7d
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 20:52:52 +0100

    Created BookingController and added CreateBooking

---
+++ Rental.API/Controllers/BookingController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Models.Requests;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/booking")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService bookingService;
+
+        public BookingController(IBookingService bookingService)
+        {
+            this.bookingService = bookingService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingRequest request)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await bookingService.CreateBookingAsync(request, userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
