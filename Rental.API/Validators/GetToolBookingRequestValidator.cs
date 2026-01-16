b9c54590ffd4309e029a1de425f93fd45edbfae6
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 22:46:59 +0100

    Created GetToolBookingRequestValidator

---
+++ Rental.API/Validators/GetToolBookingRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class GetToolBookingRequestValidator : AbstractValidator<GetToolBookingRequest>
+    {
+        public GetToolBookingRequestValidator() {
+            RuleFor(x => x.ToolId).GreaterThan(0);
+        }
+    }
+}
