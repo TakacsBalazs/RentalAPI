f07e23d2336adaa8e3e81640e6ed444caa59be1a
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 20:51:38 +0100

    Created CreateBookingRequest

---
+++ Rental.API/Models/Requests/CreateBookingRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Rental.API.Models.Requests
+{
+    public class CreateBookingRequest
+    {
+        public int ToolId { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+    }
+}
