00c02510b40ced259e254d7e859fc3f2635d6d2d
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-16 20:51:58 +0100

    Created IBookingService and added CreateBookingAsync

---
+++ Rental.API/Services/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using Rental.API.Common;
+using Rental.API.Models;
+using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public interface IBookingService
+    {
+        Task<Result<BookingResponse>> CreateBookingAsync(CreateBookingRequest request, string renterId);
+    }
+}
