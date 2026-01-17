1a66ddd402caa258e79278a161d5c42c82ed327a
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-17 19:16:13 +0100

    Created StartBookingRequestValidator

---
+++ Rental.API/Validators/StartBookingRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class StartBookingRequestValidator : AbstractValidator<StartBookingRequest>
+    {
+        public StartBookingRequestValidator() {
+            RuleFor(x => x.PickupCode).NotEmpty();
+        }
+    }
+}
