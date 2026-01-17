637048e1a5bb0898fadebb90b72dd227cbbc54a6
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-17 23:12:00 +0100

    Created AddOriginalEndDateToBookings

---
+++ Rental.API/Migrations/20260117215828_AddOriginalEndDateToBookings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOriginalEndDateToBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "OriginalEndDate",
+                table: "Bookings",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OriginalEndDate",
+                table: "Bookings");
+        }
+    }
+}
