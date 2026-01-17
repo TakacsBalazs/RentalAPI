79f1009a637bf868f273d5c6cc5e9fbd73550c7f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-17 19:15:05 +0100

    Created AddPickupCodeAndFailedPickupAttemptsAndIsLockedToBookings migration

---
+++ Rental.API/Migrations/20260117174748_AddPickupCodeAndFailedPickupAttemptsAndIsLockedToBookings.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPickupCodeAndFailedPickupAttemptsAndIsLockedToBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FailedPickupAttempts",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                table: "Bookings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PickupCode",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedPickupAttempts",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "PickupCode",
+                table: "Bookings");
+        }
+    }
+}
