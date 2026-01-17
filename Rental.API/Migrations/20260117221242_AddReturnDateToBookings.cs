f0e0e9fe68d3d4f2da92a1c40659a8b3a94a3761
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-17 23:13:50 +0100

    Created AddReturnDateToBookings

---
+++ Rental.API/Migrations/20260117221242_AddReturnDateToBookings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReturnDateToBookings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReturnDate",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReturnDate",
+                table: "Bookings");
+        }
+    }
+}
