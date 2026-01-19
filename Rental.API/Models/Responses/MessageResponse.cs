cd108b1a7edf04fe08ae4eeea14cdffefaa92463
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-19 19:09:48 +0100

    Created MessageResponse

---
+++ Rental.API/Models/Responses/MessageResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class MessageResponse
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; } = string.Empty;
+        public string SenderName { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
