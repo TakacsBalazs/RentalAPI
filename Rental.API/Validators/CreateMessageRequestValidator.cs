c15f4ff091c72906100e78386247f2fdb057d1e5
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-19 19:10:15 +0100

    Created CreateMesssageRequestValidator

---
+++ Rental.API/Validators/CreateMessageRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class CreateMessageRequestValidator :  AbstractValidator<CreateMessageRequest>
+    {
+        public CreateMessageRequestValidator() { 
+            RuleFor(x => x.Text).NotEmpty();
+        }
+
+    }
+}
