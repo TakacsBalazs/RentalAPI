7e4fcaf6c6cab6a5da4ccc364713663dd25fe5cb
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-20 12:19:58 +0100

    Created CreateRatingRequest

---
+++ Rental.API/Models/Requests/CreateRatingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental.API.Models.Requests
+{
+    public class CreateRatingRequest
+    {
+        public string RatedUserId { get; set; } = string.Empty;
+        public int Rate { get; set; }
+        public string? Comment { get; set; }
+    }
+}
