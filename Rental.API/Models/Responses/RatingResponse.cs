c24756f1723b3f945bd4c16ec3d8a4663a22fa07
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-20 12:19:31 +0100

    Created RatingResponse

---
+++ Rental.API/Models/Responses/RatingResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class RatingResponse
+    {
+        public string RaterId { get; set; } = string.Empty;
+        public string RaterName { get; set;} = string.Empty;
+
+        public int Rate { get; set; }
+        public string? Comment { get; set; }
+        public double NewAverageRating { get; set; }
+        public int NewRatingCount { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
