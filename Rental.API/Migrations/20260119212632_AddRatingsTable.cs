333b78fc5dfce6a969f85cd04efa20e788f67420
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-20 12:18:03 +0100

    Created AddRatingsTable migration

---
+++ Rental.API/Migrations/20260119212632_AddRatingsTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRatingsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    RatedUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RaterUserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Rate = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => new { x.RatedUserId, x.RaterUserId });
+                    table.ForeignKey(
+                        name: "FK_Ratings_AspNetUsers_RatedUserId",
+                        column: x => x.RatedUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Ratings_AspNetUsers_RaterUserId",
+                        column: x => x.RaterUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_RaterUserId",
+                table: "Ratings",
+                column: "RaterUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
