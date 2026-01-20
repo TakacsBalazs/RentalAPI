60061d70c77e26b2edf05bc66c24da63fc837f58
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-20 12:24:42 +0100

    Created RatingController and added CreateRating

---
+++ Rental.API/Controllers/RatingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Models.Requests;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/ratings")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        private readonly IRatingService ratingService;
+        public RatingController(IRatingService ratingService)
+        {
+            this.ratingService = ratingService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> CreateRating([FromBody] CreateRatingRequest request)
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await ratingService.CreateRatingAsync(request, userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
