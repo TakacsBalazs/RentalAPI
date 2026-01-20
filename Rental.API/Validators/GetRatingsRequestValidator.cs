303e6cf5ee7acf416c271e577eb2c1ebd2ad4daa
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-20 12:26:02 +0100

    Created GetRatingsRequestValidator

---
+++ Rental.API/Validators/GetRatingsRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class GetRatingsRequestValidator : AbstractValidator<GetRatingsRequest>
+    {
+        public GetRatingsRequestValidator() { 
+            RuleFor(x => x.RatedUserId).NotEmpty();
+        }
+    }
+}
