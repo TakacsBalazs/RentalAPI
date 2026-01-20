4ce0e240f00525567ee148df8f97b5f7e2c2586e
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-20 12:17:06 +0100

    Created Rating model

---
+++ Rental.API/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿namespace Rental.API.Models
+{
+    public class Rating
+    {
+        public string RatedUserId { get; set; }
+        public User RatedUser { get; set; }
+        public string RaterUserId { get; set; }
+        public User RaterUser { get; set; }
+        public int Rate { get; set; }
+        public string? Comment { get; set;}
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
