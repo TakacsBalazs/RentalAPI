2c28ebdf4f1a2aa1238cc4b33c638fe7d1d2f84c
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-21 21:16:19 +0100

    IPaymentService: added CancellationUnlockBookingAmountAsync

--- Rental.API/Services/IPaymentService.cs
+++ Rental.API/Services/IPaymentService.cs
@@ -5,5 +5,7 @@ namespace Rental.API.Services
     public interface IPaymentService
     {
         Task<Result> LockBookingAmountAsync(string userId, decimal amount, int bookingId);
+
+        Task<Result> CancellationUnlockBookingAmountAsync(string userId, decimal amount, int bookingId);
     }
 }
