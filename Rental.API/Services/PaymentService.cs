296e83a54d134531682235e0a7e5a447bea2b019
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-21 20:05:51 +0100

    Created PaymentService and added LockBookingAmountAsync

---
+++ Rental.API/Services/PaymentService.cs
@@ -0,0 +1,48 @@
+﻿using Rental.API.Common;
+using Rental.API.Data;
+using Rental.API.Models.Responses;
+using Rental.API.Models;
+
+namespace Rental.API.Services
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly AppDbContext context;
+        public PaymentService(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Result> LockBookingAmountAsync(string userId, decimal amount, int bookingId)
+        {
+            var user = await context.Users.FindAsync(userId)!;
+            if (user == null)
+            {
+                return Result.Failure("Invalid Renter User Id!");
+            }
+
+            if (user.Balance < amount)
+            {
+                return Result.Failure("Renter doesn't have enough money!");
+            }
+
+            user.Balance -= amount;
+            user.LockedBalance += amount;
+
+            var transaction = new Transaction
+            {
+                UserId = userId,
+                Amount = amount,
+                Type = TransactionType.BookingLock,
+                BookingId = bookingId,
+                Description = $"Lock booking payment (#{bookingId})",
+                BalanceSnapshot = user.Balance,
+                LockedBalanceSnapshot = user.LockedBalance
+            };
+            context.Transactions.Add(transaction);
+
+            await context.SaveChangesAsync();
+
+            return Result.Success();
+        }
+    }
+}
