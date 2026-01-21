bce33656d44a1b1cd22fc219441df284c49a36d7
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-21 20:05:42 +0100

    Created IPaymentService and added LockBookingAmountAsync

---
+++ Rental.API/Services/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using Rental.API.Common;
+
+namespace Rental.API.Services
+{
+    public interface IPaymentService
+    {
+        Task<Result> LockBookingAmountAsync(string userId, decimal amount, int bookingId);
+    }
+}
