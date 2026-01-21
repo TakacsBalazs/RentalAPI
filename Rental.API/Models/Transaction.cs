8a29d8b754f528e0468b350cd91e0dcc69c0880f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-21 19:39:57 +0100

    Created Transaction model

---
+++ Rental.API/Models/Transaction.cs
@@ -0,0 +1,39 @@
+﻿using Rental.API.Common;
+
+namespace Rental.API.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public User User { get; set; }
+
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; } = string.Empty;
+
+        public int? BookingId { get; set; }
+        public Booking? Booking { get; set; }
+
+        public decimal BalanceSnapshot { get; set; }
+        public decimal LockedBalanceSnapshot { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        BalanceTopUp = 1,
+        Withdraw = 2,
+
+        BookingLock = 10,
+        BookingCancellationUnlock = 11,
+
+        RentalFeePayment = 20,
+        RentalIncome = 21,
+        SecurityDepositRefund = 22,
+
+        DamagePenalty = 30,
+        PenaltyIncome = 31,
+    }
+}
