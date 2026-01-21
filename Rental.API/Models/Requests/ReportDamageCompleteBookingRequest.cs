8090e2ddd57bc809627f576642cd7a7381be7e4c
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-22 00:31:42 +0100

    Created ReportDamageCompleteBookingRequest

---
+++ Rental.API/Models/Requests/ReportDamageCompleteBookingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Rental.API.Models.Requests
+{
+    public class ReportDamageCompleteBookingRequest
+    {
+        public decimal DamageAmount { get; set; }
+
+        public string DamageDescription { get; set; } = string.Empty;
+    }
+}
