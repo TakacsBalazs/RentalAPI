93da106446388e215209c7136a31d3eb622bdb5c
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-22 00:30:08 +0100

    Created ReportDamageCompleteBookingResponse

---
+++ Rental.API/Models/Responses/ReportDamageCompleteBookingResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class ReportDamageCompleteBookingResponse
+    {
+        public decimal PaidDamage { get; set; }
+
+        public decimal RequestedDamage { get; set; }
+
+        public bool IsFullyCovered { get; set; }
+    }
+}
