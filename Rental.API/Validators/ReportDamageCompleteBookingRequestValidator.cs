517729d85944c3557a54dbfb5c0b2b61d4b31ea8
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-22 00:32:20 +0100

    Created ReportDamageCompleteBookingRequestValidator

---
+++ Rental.API/Validators/ReportDamageCompleteBookingRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class ReportDamageCompleteBookingRequestValidator : AbstractValidator<ReportDamageCompleteBookingRequest>
+    {
+        public ReportDamageCompleteBookingRequestValidator()
+        {
+            RuleFor(x => x.DamageAmount).GreaterThan(0);
+            RuleFor(x => x.DamageDescription).NotEmpty().MaximumLength(1000);
+        }
+    }
+}
