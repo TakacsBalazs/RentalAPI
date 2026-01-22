4d4e16240d0842946eb5bcf1914003301c986408
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-22 16:41:11 +0100

    Created TransactionController and added GetUserTransactions (/transactions)

---
+++ Rental.API/Controllers/TransactionController.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/transactions")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService transactionService;
+        public TransactionController(ITransactionService transactionService)
+        {
+            this.transactionService = transactionService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserTransactions()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await transactionService.GetUserTransactionsAsync(userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
