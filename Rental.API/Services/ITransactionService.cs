6fe6d66184e7fa6810004ce2759bee00fd2f1ee0
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-22 16:38:59 +0100

    ITransactionService: added GetUserTransactionsAsync

---
+++ Rental.API/Services/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using Rental.API.Common;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public interface ITransactionService
+    {
+        Task<Result<IEnumerable<TransactionResponse>>> GetUserTransactionsAsync(string userId);
+    }
+}
