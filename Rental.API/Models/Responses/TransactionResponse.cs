08805aa6350882886c90677ca31ef3481ce6de04
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-22 16:39:39 +0100

    Created TransactionResponse

---
+++ Rental.API/Models/Responses/TransactionResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class TransactionResponse
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public string Description { get; set; } = string.Empty;
+
+        public int? BookingId { get; set; }
+
+        public decimal BalanceSnapshot { get; set; }
+        public decimal LockedBalanceSnapshot { get; set; }
+    }
+}
