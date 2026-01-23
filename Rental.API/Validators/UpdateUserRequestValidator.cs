9936484b65b38b4ebee3cf89862a5e83f12832a3
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 13:05:14 +0100

    Created UpdateUserRequestValidator

---
+++ Rental.API/Validators/UpdateUserRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class UpdateUserRequestValidator : AbstractValidator<UpdateUserRequest>
+    {
+        public UpdateUserRequestValidator() {
+            RuleFor(x => x.FullName).NotEmpty().MaximumLength(100);
+        }
+    }
+}
