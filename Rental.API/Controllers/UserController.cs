eb5a274b24512da6fe19dd26886a48d5b0e0de8d
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 13:01:44 +0100

    Created UserController and added GetOwnProfile (users/profile route)

---
+++ Rental.API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Common;
+using Rental.API.Models;
+using Rental.API.Models.Requests;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [Authorize]
+        [HttpGet("profile")]
+        public async Task<IActionResult> GetOwnProfile()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await userService.GetOwnProfileAsync(userId);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
