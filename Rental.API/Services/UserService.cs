6e17d0875557dc2f3a4b1752a8f2c0683d486aff
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 13:00:22 +0100

    Created UserService and added GetOwnProfileAsync

---
+++ Rental.API/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rental.API.Common;
+using Rental.API.Data;
+using Rental.API.Extensions;
+using Rental.API.Models.Requests;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext context;
+        private readonly IServiceProvider serviceProvider;
+        public UserService(AppDbContext context , IServiceProvider serviceProvider)
+        {
+            this.context = context;
+            this.serviceProvider = serviceProvider;
+        }
+
+        public async Task<Result<MyProfileResponse>> GetOwnProfileAsync(string userId)
+        {
+            var user = await context.Users.FindAsync(userId);
+            if(user == null)
+            {
+                return Result<MyProfileResponse>.Failure("Invalid User Id!");
+            }
+
+            var response = new MyProfileResponse
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                Username = user.UserName!,
+                CreatedAt = user.CreatedAt,
+                Email = user.Email!,
+                Balance = user.Balance,
+                LockedBalance = user.LockedBalance,
+            };
+
+            return Result<MyProfileResponse>.Success(response);
+        }
+    }
+}
