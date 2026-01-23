b84c8fe4497526bf2f136e469d7b1dad3071cfdd
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 13:02:53 +0100

    IUserService: Added GetUserByIdAsync

--- Rental.API/Services/IUserService.cs
+++ Rental.API/Services/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Rental.API.Common;
+using Rental.API.Models.Requests;
 using Rental.API.Models.Responses;
 
 namespace Rental.API.Services
@@ -6,5 +7,6 @@ namespace Rental.API.Services
     public interface IUserService
     {
         Task<Result<MyProfileResponse>> GetOwnProfileAsync(string userId);
+        Task<Result<PublicUserResponse>> GetUserByIdAsync(string userId);
     }
 }
