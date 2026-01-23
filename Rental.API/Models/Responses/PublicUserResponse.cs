e6a3f33bc8a94cf16453bfb2654c134558406ea9
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 13:02:30 +0100

    Created PublicUserResponse

---
+++ Rental.API/Models/Responses/PublicUserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class PublicUserResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public double Rate { get; set; }
+        public int RatersCount { get; set; }
+    }
+}
