c2c4a2bb4205b018b92012ada979439747a766a7
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 12:58:50 +0100

    Created IUserService and added GetOwnProfileAsync

---
+++ Rental.API/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Rental.API.Common;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public interface IUserService
+    {
+        Task<Result<MyProfileResponse>> GetOwnProfileAsync(string userId);
+    }
+}
