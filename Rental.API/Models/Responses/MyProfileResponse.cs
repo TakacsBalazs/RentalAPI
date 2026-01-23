63f5ac3f782b37b3d0e49699b4780531ea235f20
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 12:58:05 +0100

    Created MyProfileResponse

---
+++ Rental.API/Models/Responses/MyProfileResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Rental.API.Models.Responses
+{
+    public class MyProfileResponse
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public decimal Balance { get; set; }
+        public decimal LockedBalance { get; set; }
+
+    }
+}
