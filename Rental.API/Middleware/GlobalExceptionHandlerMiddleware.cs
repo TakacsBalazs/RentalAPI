a7698e3d7857a6c137172ce6177320a45904db10
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 16:05:08 +0100

    Created GlobalExceptionHandleMiddleware

---
+++ Rental.API/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.Json;
+using System;
+
+namespace Rental.API.Middleware
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> logger;
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            { 
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Internal Server Error: {Message}", ex.Message);
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var response = new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = "Internal Server Error. Please try again later.",
+                };
+
+                var json = JsonSerializer.Serialize(response);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
