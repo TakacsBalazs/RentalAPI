5859d6f1bb7c73499b399ed692678e0d2823d17f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-23 13:13:37 +0100

    Created AddDescriptionToUsers migration

---
+++ Rental.API/Migrations/20260123121102_AddDescriptionToUsers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rental.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "AspNetUsers",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "AspNetUsers");
+        }
+    }
+}
