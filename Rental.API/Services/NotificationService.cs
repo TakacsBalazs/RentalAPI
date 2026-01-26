80ca2b38b33a31c69b1af66e772436eda7cb845b
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-26 15:30:07 +0100

    Created NotificationService and added GetUserNotificationsAsync

---
+++ Rental.API/Services/NotificationService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rental.API.Common;
+using Rental.API.Data;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly AppDbContext context;
+        public NotificationService(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<Result<IEnumerable<NotificationResponse>>> GetUserNotificationsAsync(string userId)
+        {
+            var response = await context.Notifications.Where(x => x.UserId == userId).OrderByDescending(x => x.CreatedAt).Select(x => new NotificationResponse
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Message = x.Message,
+                IsRead = x.IsRead,
+                CreatedAt = x.CreatedAt,
+            }).ToListAsync();
+            return Result<IEnumerable<NotificationResponse>>.Success(response);
+        }
+    }
+}
