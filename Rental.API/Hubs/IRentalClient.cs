f02923ad4bad749ebc11a481437796f76683ccfb
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-26 19:38:36 +0100

    Created IRentalClient and added ReceiveMessage, MesseageRead, ReceiveNotification

---
+++ Rental.API/Hubs/IRentalClient.cs
@@ -0,0 +1,12 @@
+﻿using Rental.API.Models.Responses;
+
+namespace Rental.API.Hubs
+{
+    public interface IRentalClient
+    {
+        Task ReceiveMessage(MessageResponse message);
+        Task MessageRead(int messageId);
+
+        Task ReceiveNotification(NotificationResponse notification);
+    }
+}
