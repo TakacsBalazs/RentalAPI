23983e6b6d5ff2b325b3481f1dd1d05ce149412a
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-26 15:30:39 +0100

    Created NotificationController and added GetUserNotification

---
+++ Rental.API/Controllers/NotificationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rental.API.Services;
+using System.Security.Claims;
+
+namespace Rental.API.Controllers
+{
+    [Route("api/notifications")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService notificationSercvice;
+        public NotificationController(INotificationService notificationSercvice)
+        {
+            this.notificationSercvice = notificationSercvice;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserNotifications()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+            var result = await notificationSercvice.GetUserNotificationsAsync(userId);
+            if(!result.IsSuccess)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok(result.Data);
+        }
+    }
+}
