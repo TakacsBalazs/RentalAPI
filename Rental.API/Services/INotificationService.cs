1b94c9fc598f43edc79434a509cc3aa3667eb70f
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-01-26 15:29:46 +0100

    Created INotificationService and added GetUserNotificationsAsync

---
+++ Rental.API/Services/INotificationService.cs
@@ -0,0 +1,10 @@
+﻿using Rental.API.Common;
+using Rental.API.Models.Responses;
+
+namespace Rental.API.Services
+{
+    public interface INotificationService
+    {
+        Task<Result<IEnumerable<NotificationResponse>>> GetUserNotificationsAsync(string userId);
+    }
+}
