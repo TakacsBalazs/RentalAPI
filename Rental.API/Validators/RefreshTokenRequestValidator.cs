e6cbfdf2d4f1f28a358f286d65b894a5ad765464
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-02-09 15:29:38 +0100

    Created RefreshTokenRequestValidator

---
+++ Rental.API/Validators/RefreshTokenRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class RefreshTokenRequestValidator : AbstractValidator<RefreshTokenRequest>
+    {
+        public RefreshTokenRequestValidator() {
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+}
