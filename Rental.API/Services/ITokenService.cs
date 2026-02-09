22c3bf94f4c6d9aef4579950e2a90f01c299fee2
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-02-09 15:25:47 +0100

    Created ITokenService and added GenerateJwtToken, GenerateRefreshToken

---
+++ Rental.API/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using Rental.API.Models;
+
+namespace Rental.API.Services
+{
+    public interface ITokenService
+    {
+        string GenerateJwtToken(User user);
+
+        RefreshToken GenerateRefreshToken(string userId);
+    }
+}
