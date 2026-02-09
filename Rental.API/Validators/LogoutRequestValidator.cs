c13783816ac88bed4fd21f6bccf5f58e3ab133b1
TakacsBalazs <neu.takacs.balazs1@gmail.com>
2026-02-09 17:07:09 +0100

    Created LogoutRequestValidator

---
+++ Rental.API/Validators/LogoutRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Rental.API.Models.Requests;
+
+namespace Rental.API.Validators
+{
+    public class LogoutRequestValidator : AbstractValidator<LogoutRequest>
+    {
+        public LogoutRequestValidator() { 
+            RuleFor(x => x.RefreshToken).NotEmpty();
+        }
+    }
+}
